083ded6edc04e1e41e3ebc8fd8366a2296fed0fb
Admin <admin@Admins-MacBook-Air.local>
2025-08-04 16:15:48 +0530

    UI: Updated sidebar submenu colors and added logo

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -18,35 +18,37 @@ export function Sidebar() {
   },[user])
   return (
     <ShadcnSidebar side="left" collapsible="icon" variant="sidebar">
-      <SidebarHeader className="p-4 space-y-0">
-        <div className="flex items-center space-x-2">
+      <SidebarHeader className="p-4 space-y-0" style={{ backgroundColor: "white",color:"black" }}>
+        <div className="flex items-center space-x-2" style={{ backgroundColor: "white" }}>
           <img
-            src="/fandoro-logo.png"
-            alt="Fandoro Technologies"
+            src="/logo/logo_no_text_500x500.png"
+            alt="Fandoro"
             className="h-8 w-8"
           />
           <div>
-            <h2 className="text-xl font-bold text-sidebar-foreground">
-              ESG Monitor{import.meta.env.VITE_APP_ENV !== 'production' ? ` (${import.meta.env.VITE_APP_ENV})` : ''}
-            </h2>
-            <p className="text-sm text-sidebar-foreground/70">Fandoro Technologies</p>
+          <img src="/logo/logo_text_only_700x150.png" alt="Fandoro" className="h-7" />
+            {/* <h2 className="text-xl font-bold text-sidebar-foreground">
+              {import.meta.env.VITE_APP_ENV !== 'production' ? ` (${import.meta.env.VITE_APP_ENV})` : ''}
+            </h2> */}
+            {/* <p className="text-sm text-sidebar-foreground/70">Fandoro Technologies</p> */}
           </div>
         </div>
       </SidebarHeader>
 
-      <SidebarContent className="mt-4">
+      <SidebarContent className="bg-white text-black border border-gray-300">
         <SidebarNavigation />
       </SidebarContent>
 
-      <SidebarFooter className="p-4 text-xs text-sidebar-foreground/70">
-        <div className="flex items-center space-x-2">
+      <SidebarFooter className="text-black p-4 bg-white text-xs text-sidebar-foreground/70">
+        <div className="flex items-center space-x-2 text-black" >
           <img
-            src="/fandoro-logo.png"
+            src="/logo/logo_no_text_500x500.png"
             alt="Fandoro Technologies"
             className="h-6 w-6"
           />
           <div>
-            <p>© {new Date().getFullYear()} Fandoro Technologies</p>
+            <p>Copyright ©{new Date().getFullYear()} Fandoro Technologies</p>
+              {/* <p></p> */}
             <p>All rights reserved</p>
           </div>
         </div>
